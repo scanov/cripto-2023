64bbc83e76fe433ea9735f132e2bbe7ad606d404
eporrasm <vals4oo@gmail.com>
2023-11-20 15:51:46 -0500

    Update settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default=os.environ.get('DATABASE_URL', default=os.environ.get('STR_CONNECTION')),
         conn_max_age=600
     )
 }
