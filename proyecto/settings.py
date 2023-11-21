2ce27c9b51c6c7c48b2c0bbc465793b7d90bd844
eporrasm <vals4oo@gmail.com>
2023-11-20 21:35:43 -0500

    ssh1 endpoint

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -10,9 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
 import os
 import dj_database_url
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
